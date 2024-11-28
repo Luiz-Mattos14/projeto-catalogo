b78d95d48c2d5bf1087064ee960199f9cf1cc891
Luiz Mattos <luiz.mattos@vnda.com.br>
2024-11-28 19:29:27 -0300

    Remover node-modules

--- config.php
+++ config.php
@@ -5,23 +5,13 @@ require 'environment.php';
 $config = array();
 
 
-if (ENVIRONMENT == 'development') {
-  define("BASE_URL", "http://localhost/projeto-catalogo/");
-  define("PATH_SITE", "./");
+define("BASE_URL", "http://localhost/projeto-catalogo/");
+define("PATH_SITE", "./");
 
-  $config['dbname'] = 'catalogo_database';
-  $config['host'] = '127.0.0.1'; 
-  $config['dbuser'] = 'root';
-  $config['dbpass'] = '';
-} else {
-  define("BASE_URL", "https://devluizmattos.com.br/");
-  define("PATH_SITE", "./");
-
-  $config['dbname'] = 'devlui46_catalogo_database';
-  $config['host'] = 'localhost'; 
-  $config['dbuser'] = 'devlui46_luimattos';
-  $config['dbpass'] = 'v[!7JF9k?[g+';
-}
+$config['dbname'] = 'catalogo_database';
+$config['host'] = '127.0.0.1'; 
+$config['dbuser'] = 'root';
+$config['dbpass'] = '';
 
 global $db;
 try {
